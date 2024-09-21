c1431107c2b4b6f129211a24997ac553e5959cbe
unknown <Administrator@adm-1005-abdo.BUC.local>
2024-09-21 05:57:39 +0200

    project done

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,34 @@
+import React from 'react';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
+//...
+import * as Sentry from "@sentry/react";
+
+
+Sentry.init({
+  dsn: "https://4414c3b5f61f04a5dd20de114d810cc9@o4507947722145792.ingest.de.sentry.io/4507947737677904",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    // Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.browserProfilingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  
+  profilesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+});
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
